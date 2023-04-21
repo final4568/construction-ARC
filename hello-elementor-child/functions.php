8bb29c5440777c0bfd586634c854a09089ed9f36
Usama Ali <85936445+final4568@users.noreply.github.com>
2023-04-21 17:05:28 +0500

    first commit

---
+++ hello-elementor-child/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Theme functions and definitions
+ *
+ * @package HelloElementorChild
+ */
+
+/**
+ * Load child theme css and optional scripts
+ *
+ * @return void
+ */
+
+
+function hello_elementor_child_enqueue_scripts() {
+    // Enqueue parent stylesheet
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    // Enqueue child stylesheet
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/assets/css/main.css', array( 'parent-style' ), '1.0.0' );
+    
+    // Enqueue custom JavaScript file
+    wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_scripts' );
+
